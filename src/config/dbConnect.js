d34e25f4a5ab0533c3760e30f0fdc8edbf3f9443
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-14 19:23:46 -0300

    feat(db) connection to database

---
+++ src/config/dbConnect.js
@@ -0,0 +1,42 @@
+import { MongoClient } from 'mongodb';
+import "dotenv/config";
+
+// export default async function connectToDatabase() {
+//   try {
+//     if(!process.env.DB_CONNECTION_STRING) {
+//       throw new Error('DB_CONNECTION_STRING is not defined in .env file');
+//     }
+//     const client = new MongoClient(process.env.DB_CONNECTION_STRING)
+//     await client.connect();
+//     const db = client.db('realtime-websockets');
+//     const documents = db.collection('documents');
+//     if(documents){
+//       consol.log('Connected to MongoDB');
+//     }
+
+//   } catch (error) {
+//     consol.elog(error)
+//   }
+// }
+
+const client = new MongoClient(process.env.DB_CONNECTION_STRING)
+
+let documentsCollection;
+
+try {
+  if (!process.env.DB_CONNECTION_STRING) {
+    throw new Error('DB_CONNECTION_STRING is not defined in .env file');
+  }
+  await client.connect();
+  const db = client.db('realtime-websockets');
+  documentsCollection = db.collection('documents');
+
+  if (documentsCollection) {
+    console.log('Connected to Data base');
+  }
+} catch (error) {
+  consol.log(error)
+}
+
+export { documentsCollection }
+
