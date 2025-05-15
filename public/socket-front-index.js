bd6351ec9dc19233efaff4e9091ea3509354228d
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-15 17:24:13 -0300

    feat(socket-front) lintening events like docuemnt exist and was delete

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -1,6 +1,6 @@
 // esse arquivo faz referencia ao index.js
 
-import { insertLinkDocument } from "./index.js"
+import { insertLinkDocument, removeLinkDocument } from "./index.js"
 
 const socket = io()
 
@@ -20,4 +20,18 @@ export function createNewDocument(documentName) {
 // irá escutar o evento que caso seja criado um novo documento, ele irpa inserir o link do documento na tela sem ter que dar reload
 socket.on("add_newDocument_interface", (documentName) => {
   insertLinkDocument(documentName)
+})
+// 
+
+
+// irá escurtar o evento caso o documento já exista
+socket.on("document_already_exists", (documentName) => {
+  alert(`O documento ${documentName} já existe!`)
+})
+
+
+// para que possamos atualizar a página inicial com os documentos existente, iremos escutrar o evento de quando um documento é excluido com sucesso
+socket.on("delete_document_success", (documentName) => {
+  // e apenas chamamos a func que irá atualizar o html com o valor recebido pelo servidor
+  removeLinkDocument(documentName)
 })
