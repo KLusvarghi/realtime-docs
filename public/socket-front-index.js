78f1a7d77f835ef9cf9c4b03589881ffccd20299
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-15 15:40:14 -0300

    feat(socket-front) new function to emit socket event if had been created new document and is listening a event and insert the new document on interface for everyone on server

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -4,10 +4,20 @@ import { insertLinkDocument } from "./index.js"
 
 const socket = io()
 
-// emitindo um evento para o servidor
+// emitindo um evento para obter os documentos existentes no banco
 socket.emit('get_documents', (documents) => {
-  console.log(documents)
+  // console.log(documents)
   documents.forEach((document) => {
     insertLinkDocument(document.name)
   });
+})
+
+export function createNewDocument(documentName) {
+  // emitindo um evento para o servidor
+  socket.emit('new_document', documentName)
+}
+
+// irá escutar o evento que caso seja criado um novo documento, ele irpa inserir o link do documento na tela sem ter que dar reload
+socket.on("add_newDocument_interface", (documentName) => {
+  insertLinkDocument(documentName)
 })
