a73b76971886f69bce96202c029c82006a62428b
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-15 17:21:33 -0300

    feat(remove) add func to remove  documento on screen after delete

--- public/index.js
+++ public/index.js
@@ -8,17 +8,27 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   // emitindo o evento para o servidor
-    createNewDocument(newDocumentInput.value)
+  createNewDocument(newDocumentInput.value)
   // limpando o input
   newDocumentInput.value = '';
 })
 
 export function insertLinkDocument(documentName) {
   documentList.innerHTML += `
-    <a href="documento.html?nome=${documentName}" class="list-group-item list-group-item-action">
+    <a href="documento.html?nome=${documentName}" class="list-group-item list-group-item-action" id="document-${documentName}">
       ${documentName}
     </a>
   `
+}
+
+// após remover o docuemntos, temos que tirar ele da tela, e pra isso:
+export function removeLinkDocument(documentName) {
+  // podendo pegar pelas duas maneiras:
+  // const documentLink = document.querySelector(`a[href="documento.html?nome=${documentName}"]`)
+  const documentLink = document.getElementById(`document-${documentName}`)
+  if (documentLink) {
+    documentList.removeChild(documentLink)
+  }
 }
 
 
